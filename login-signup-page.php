9d22153a579234851a47c3d1aee60c4e5dff7ddc
MasterKoopaan <32260048+MasterKoopaan@users.noreply.github.com>
2018-01-23 15:53:18 +0100

    working on the login page

--- login-signup-page.php
+++ login-signup-page.php
@@ -19,10 +19,21 @@
 
       <div id="container-log-sign">
 
-        <div class="bar login bar-display">
-
+        <div class="block login block-display">
+          <div>
+            <h1>Login</h1>
+            <p>korte informatie. join puzlo en krijg bereik tot vele voordelen in je dagelijkse shopehlic life!</p>
+            <form class="form form-login" action="php/.....php" method="POST">
+              <p class="titel-input">Email or Username:</p>
+              <input type="text" name="accountidname" placeholder="Email/Username">
+              <p class="titel-input">Password:</p>
+              <input type="password" name="pwd" placeholder="Password">
+              <br>
+              <button type="submit" name="sumit">Login</button>
+            </form>
+          </div>
         </div>
-        <div class="bar sign-up">
+        <div class="block sign-up">
 
         </div>
       </div>
