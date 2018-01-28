0c794d7011bf669fd760e44da98249698218589a
mastermaarten <mastermaarten99@gmail.com>
2018-01-28 22:16:11 +0100

    html versie

--- php/header.php
+++ php/header.php
@@ -3,13 +3,13 @@
   <nav id="navigatie">
     <div id="header-nav">
       <ul>
-        <li><a href="index.php">Home</a></li>
+        <li><a href="index.html">Home</a></li>
         <li><a href="#">About</a>
           <ul>
-            <li><a href="about.php#puzlo">Puzlo</a></li>
-            <li><a href="login-signup-page.php">Signup</a></li>
-            <li><a href="bedrijfaccounthomepage.php">FAQ</a></li>
-            <li><a href="about-join.php">Join</a></li>
+            <li><a href="about.html#puzlo">Puzlo</a></li>
+            <li><a href="login-signup-page.html">Signup</a></li>
+            <li><a href="bedrijfaccounthomepage.html">FAQ</a></li>
+            <li><a href="about-join.html">Join</a></li>
           </ul>
         </li>
         <li><a href="#">Deelnemers</a></li>
@@ -26,8 +26,8 @@
 
   <div id="header-user">
     <div id="user-login">
-      <a href="login-signup-page.php?state=login" class="button-nav login">Login</a>
-      <a href="login-signup-page.php?state=sign" class="button-nav signup">Sign up</a>
+      <a href="login-signup-page.html" class="button-nav login">Login</a>
+      <a href="login-signup-page.html" class="button-nav signup">Sign up</a>
     </div>
     <div id="user-loggedin"></div>
   </div>
